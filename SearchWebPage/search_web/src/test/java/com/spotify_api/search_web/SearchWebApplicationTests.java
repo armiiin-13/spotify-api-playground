027fb2c1f083fe9d7a8581b2fb3f900a94826829
armin <armingarc@yahoo.com>
2025-11-24 21:39:15 +0100

    SpringBootApp created and setted up

---
+++ SearchWebPage/search_web/src/test/java/com/spotify_api/search_web/SearchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify_api.search_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

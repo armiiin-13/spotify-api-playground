0bce491302c2afa81b02d926ae4ff7b9d1e084a4
armin <armingarc@yahoo.com>
2025-11-27 00:28:50 +0100

    Test: Controller/ index.html (remember)

--- SearchWebPage/search_web/src/main/java/com/spotify_api/search_web/controller/MainController.java
+++ SearchWebPage/search_web/src/main/java/com/spotify_api/search_web/controller/MainController.java
@@ -1,8 +1,22 @@
 package com.spotify_api.search_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String getSearch(Model model, @RequestParam(required=false) String search) {
+        if (search != null && !search.trim().isEmpty()){
+            model.addAttribute("search",search);
+        } else {
+            model.addAttribute("search", null);
+        }
+        return "index";
+    }
     
+
 }
